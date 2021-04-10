d600a30fa80797b113eb35ced31aa03fd673eb6c
chpprogramador <82235083+chpprogramador@users.noreply.github.com>
2021-04-10 15:42:01 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 
 /** O nome do banco de dados */
-define('DB_NAME', 'kabum');
+define('DB_NAME', '');
 
 /** Usuário do banco de dados MySQL */
-define('DB_USER', 'kabum');
+define('DB_USER', '');
 
 /** Senha do banco de dados MySQL */
-define('DB_PASSWORD', 'kabum1010');
+define('DB_PASSWORD', '');
 
 /** Nome do host do MySQL */
-define('DB_HOST', 'mysql.kabum.kinghost.net');
+define('DB_HOST', '');
 
 ?>
